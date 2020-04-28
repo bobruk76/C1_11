fb39ff81d4c6231eac2af7fc69f25b038dd369ca
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-04-28 13:34:35 +0300

    buttons add

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
+function btnFn($obj, oper, minValue, maxValue){
+  let value
+  $obj.value(++$obj.value()[0]);
+}
+
 function init() {
-  const newObj = `<img class="change_this" src="https://batmanrobin.23video.com/files/11723138/1b965a5c398e48b.jpg" alt="">`;
-  const newText = `Можно поменять на этот текст`;
 
-  let msg1=$("#message1")
-  msg1.value(newObj);
-  let msg2=$("#message2");
-  msg2.value(newText);
-
-  $("#btn1").click(function(){
-    const $temp=$(".change_this").html()[0];
-    obj = $(".change_this").html(msg1.value()[0]);
-    msg1.value($temp);
+  $("#btn_min_up").click(function(){
+    $obj = $("#minutes")
+    $obj.value(++$obj.value()[0]);
   });
 
   $("#btn2").click(function(){
